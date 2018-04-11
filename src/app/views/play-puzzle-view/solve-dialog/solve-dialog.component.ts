44eef5a03e9a5e77cf3fe0bfcfa32baee53fa007
Andras Kovacs <Andras.Kovacs4@ge.com>
2018-04-11 11:02:09 +0200

    answers

--- src/app/views/play-puzzle-view/solve-dialog/solve-dialog.component.ts
+++ src/app/views/play-puzzle-view/solve-dialog/solve-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {Puzzle} from '../../../types';
+import {NotificationsService} from '../../../services/notifications/notifications.service';
+import {LevenshteinService} from '../../../services/levenshtein/levenshtein.service';
 
 @Component({
   selector: 'app-solve-dialog',
@@ -9,12 +11,14 @@ import {Puzzle} from '../../../types';
 })
 export class SolveDialogComponent implements OnInit {
 
-  solved = false;
+  solvedAnswer = null;
   answer: string;
 
   constructor(
     public dialogRef: MatDialogRef<SolveDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Puzzle
+    @Inject(MAT_DIALOG_DATA) public data: Puzzle,
+    private notificationsService: NotificationsService,
+    private levenshteinService: LevenshteinService
   ) { }
 
   onNoClick(): void {
@@ -25,8 +29,39 @@ export class SolveDialogComponent implements OnInit {
   }
 
   onSolve() {
-    const COMPARABLE = s => s.trim().toLowerCase();
-    this.solved = this.data.answers.map(COMPARABLE).includes(COMPARABLE(this.answer));
+    if (this.answer && this.answer.length) {
+      const COMPARABLE = s => s.trim().toLowerCase();
+      const comparableAnswer = COMPARABLE(this.answer);
+
+      const result = this.data.answers
+        .reduce((r, a) => {
+          const d = this.levenshteinService.levenshteinDistance(COMPARABLE(a), comparableAnswer);
+          return (d < r.distance) ? {
+            distance: d,
+            answer: a
+          } : r;
+        }, {
+          distance: 1000,
+          answer: ''
+        });
+
+      if (result.distance === 0) {
+        this.solvedAnswer = result.answer;
+      } else {
+        const feedbacks = [
+          'Forró, forró!',
+          'Majdnem!',
+          'Ez lesz az ...',
+          'Végül is ez már hasonlít...',
+          'Langyos, langyos...',
+          'Nem az igazi...',
+          'Hideg, hideg...'
+        ];
+        const feedback = feedbacks[Math.min(result.distance,feedbacks.length) - 1];
+        this.notificationsService.error(feedback);
+      }
+    }
+
   }
 }
 
