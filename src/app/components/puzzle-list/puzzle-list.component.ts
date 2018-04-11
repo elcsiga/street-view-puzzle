45ee33ff078074e93e4087699601de3406eb9c41
elcsiga home <elcsiga@gmail.com>
2018-04-11 23:07:58 +0200

    list

--- src/app/components/puzzle-list/puzzle-list.component.ts
+++ src/app/components/puzzle-list/puzzle-list.component.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs/Observable";
 import {AngularFirestore} from "angularfire2/firestore";
 import {Puzzle} from "../../types";
 import {StreetViewService} from '../../services/street-view/street-view.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-puzzle-list',
@@ -13,11 +14,12 @@ export class PuzzleListComponent implements OnInit {
 
   puzzles: Observable<Puzzle[]>;
   constructor(
-    db: AngularFirestore,
-    private streetViewService: StreetViewService
+    private db: AngularFirestore,
+    private streetViewService: StreetViewService,
+    private router: Router
   ) {
 
-    this.puzzles = db.collection('puzzles').snapshotChanges().map(actions => {
+    this.puzzles = this.db.collection('puzzles').snapshotChanges().map(actions => {
       return actions.map(a => {
         const data = a.payload.doc.data() as Puzzle;
         const id = a.payload.doc.id;
@@ -34,4 +36,7 @@ export class PuzzleListComponent implements OnInit {
     return this.streetViewService.getStaticImageUrlOf(puzzle);
   }
 
+  startPuzzle(puzzle: Puzzle) {
+    this.router.navigate(['/play/' + puzzle.id]);
+  }
 }
